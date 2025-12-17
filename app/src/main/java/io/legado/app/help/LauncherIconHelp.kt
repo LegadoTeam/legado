44bb7ab6bf6b4b2f2d813c40c0f5ffd456a3ab75
Luoyacheng <1816669175@qq.com>
2025-12-17 22:47:44 +0800

    Revert "[功能]：新增专属图标"
    This reverts commit b7594736944e4db155da9cc401c883120fec33e2.

--- app/src/main/java/io/legado/app/help/LauncherIconHelp.kt
+++ app/src/main/java/io/legado/app/help/LauncherIconHelp.kt
@@ -20,8 +20,7 @@ object LauncherIconHelp {
         ComponentName(appCtx, Launcher3::class.java.name),
         ComponentName(appCtx, Launcher4::class.java.name),
         ComponentName(appCtx, Launcher5::class.java.name),
-        ComponentName(appCtx, Launcher6::class.java.name),
-        ComponentName(appCtx, Launcher7::class.java.name)
+        ComponentName(appCtx, Launcher6::class.java.name)
     )
 
     fun changeIcon(icon: String?) {
