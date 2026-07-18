80edeaaea2cddf74522cab1cb647887727bb96df
miaogongzi <miaogongzi0227@gmail.com>
2026-07-17 23:43:41 -0400

    让滚动文本选区跟随主题强调色 (#196)
    让滚动文本选区跟随强调色

--- app/src/main/java/io/legado/app/ui/widget/text/ScrollMultiAutoCompleteTextView.kt
+++ app/src/main/java/io/legado/app/ui/widget/text/ScrollMultiAutoCompleteTextView.kt
@@ -12,6 +12,8 @@ import android.view.animation.Interpolator
 import android.widget.OverScroller
 import androidx.appcompat.widget.AppCompatMultiAutoCompleteTextView
 import androidx.core.view.ViewCompat
+import io.legado.app.lib.theme.accentColor
+import io.legado.app.utils.ColorUtils
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
@@ -81,6 +83,7 @@ open class ScrollMultiAutoCompleteTextView @JvmOverloads constructor(
         mTouchSlop = vc.scaledTouchSlop
         mMinFlingVelocity = vc.scaledMinimumFlingVelocity
         mMaxFlingVelocity = vc.scaledMaximumFlingVelocity
+        highlightColor = ColorUtils.adjustAlpha(context.accentColor, 0.35f)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
             isLocalePreferredLineHeightForMinimumUsed = false
         }
