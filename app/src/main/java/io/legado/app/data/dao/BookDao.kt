9e674eab405c3546eb42fa57b628d0a5078f8545
Jason Yao <h11129@gmail.com>
2026-08-15 12:19:24 -0700

    fix: 去掉未使用的按书名 DAO 查询
    加架按内存 trim 分组，这个精确 name 查询从未被调用，不必留在 Room 接口上。

--- app/src/main/java/io/legado/app/data/dao/BookDao.kt
+++ app/src/main/java/io/legado/app/data/dao/BookDao.kt
@@ -122,9 +122,6 @@ interface BookDao {
     @Query("SELECT * FROM books WHERE name = :name and author = :author")
     fun getBook(name: String, author: String): Book?
 
-    @Query("SELECT * FROM books WHERE name = :name")
-    fun getBooksByName(name: String): List<Book>
-
     @Query("""select distinct bs.* from books, book_sources bs 
         where origin == bookSourceUrl and origin not like '${BookType.localTag}%' 
         and origin not like '${BookType.webDavTag}%'""")
