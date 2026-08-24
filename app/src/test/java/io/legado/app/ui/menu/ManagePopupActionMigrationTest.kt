cfc67def22845781a28231e1502b7a43c77f088f
gongzi miao <miaogongzi0227@gmail.com>
2026-08-24 22:48:30 +0800

    fix: add icons to dialog overflow actions

--- app/src/test/java/io/legado/app/ui/menu/ManagePopupActionMigrationTest.kt
+++ app/src/test/java/io/legado/app/ui/menu/ManagePopupActionMigrationTest.kt
@@ -118,6 +118,32 @@ class ManagePopupActionMigrationTest {
     }
 
     @Test
+    fun `dialog menu actions provide icons for the vertical menu`() {
+        val menus = mapOf(
+            "app_log.xml" to listOf("menu_clear", "menu_export"),
+            "crash_log.xml" to listOf("menu_clear"),
+            "source_login.xml" to listOf(
+                "menu_ok",
+                "menu_show_login_header",
+                "menu_del_login_header",
+                "menu_clear_login_info",
+                "menu_log"
+            )
+        )
+        menus.forEach { (file, ids) ->
+            val source = readProjectFile("src/main/res/menu/$file")
+            ids.forEach { id ->
+                val item = source.substringAfter("android:id=\"@+id/$id\"")
+                    .substringBefore("/>")
+                assertTrue(
+                    "$file/$id should declare an icon",
+                    item.contains("android:icon=\"@drawable/")
+                )
+            }
+        }
+    }
+
+    @Test
     fun `five management adapters use the shared vertical menu`() {
         adapterFiles.forEach { path ->
             val source = readProjectFile(path)
