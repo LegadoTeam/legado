cb3b442fbab5634739dae962555dd0d522d07f29
miaogongzi <miaogongzi0227@gmail.com>
2026-09-06 01:02:26 +0800

    fix: protect long replacement rule edits
    Closes #1125

--- .github/scripts/run-ui-regression.sh
+++ .github/scripts/run-ui-regression.sh
@@ -5,5 +5,5 @@ trap 'adb pull /sdcard/Android/data/com.legado.app.debug/files/ui-regression app
 
 ./gradlew :app:connectedAppDebugAndroidTest \
   --init-script .github/scripts/source-browser-test.init.gradle \
-  -Pandroid.testInstrumentationRunnerArguments.class=io.legado.app.ui.widget.dialog.BottomWebViewDialogShowTest,io.legado.app.ui.book.explore.ExploreCategoriesTest,io.legado.app.ui.book.read.ContentEditSearchTest \
+  -Pandroid.testInstrumentationRunnerArguments.class=io.legado.app.ui.widget.dialog.BottomWebViewDialogShowTest,io.legado.app.ui.book.explore.ExploreCategoriesTest,io.legado.app.ui.book.read.ContentEditSearchTest,io.legado.app.ui.association.RuleSelectionShareTest \
   --build-cache --no-daemon --max-workers=2
