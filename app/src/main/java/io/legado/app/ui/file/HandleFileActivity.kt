cb85753f01b74fc3bfb7384c839dfc930519f5f2
miaogongzi <miaogongzi0227@gmail.com>
2026-09-07 06:08:29 +0800

    fix: support JavaScript MIME aliases in file picker (#1168)

--- app/src/main/java/io/legado/app/ui/file/HandleFileActivity.kt
+++ app/src/main/java/io/legado/app/ui/file/HandleFileActivity.kt
@@ -361,6 +361,10 @@ class HandleFileActivity :
                 when (it) {
                     "*" -> types.add("*/*")
                     "txt", "xml" -> types.add("text/*")
+                    "js" -> {
+                        types.add("application/javascript")
+                        types.add("text/javascript")
+                    }
                     else -> {
                         val mime = MimeTypeMap.getSingleton()
                             .getMimeTypeFromExtension(it)
