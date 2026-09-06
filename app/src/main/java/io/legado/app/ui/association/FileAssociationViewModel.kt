5a89c52ae0317cce93aa7dd6f752b9d66b4dbb87
gongzi miao <miaogongzi0227@gmail.com>
2026-09-07 04:03:34 +0800

    feat(import): open JavaScript source files

--- app/src/main/java/io/legado/app/ui/association/FileAssociationViewModel.kt
+++ app/src/main/java/io/legado/app/ui/association/FileAssociationViewModel.kt
@@ -7,6 +7,7 @@ import io.legado.app.R
 import io.legado.app.constant.AppLog
 import io.legado.app.constant.AppPattern
 import io.legado.app.constant.AppPattern.bookFileRegex
+import io.legado.app.constant.AppPattern.jsFileRegex
 import io.legado.app.data.entities.Book
 import io.legado.app.model.localBook.LocalBook
 import io.legado.app.utils.*
@@ -97,6 +98,10 @@ class FileAssociationViewModel(application: Application) : BaseAssociationViewMo
         }.onFailure {
             it.printOnDebug()
             AppLog.put("尝试导入为JSON文件失败\n${it.localizedMessage}", it)
+        }
+        if (fileDoc.name.matches(jsFileRegex)) {
+            successLive.postValue("bookSource" to fileDoc.uri.toString())
+            return
         }
         if (fileDoc.name.matches(bookFileRegex)) {
             importBookLiveData.postValue(fileDoc.uri)
