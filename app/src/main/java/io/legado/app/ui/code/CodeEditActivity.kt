6e967e68c127403fe73e26da061ec613ccc78ae2
gongzi miao <miaogongzi0227@gmail.com>
2026-09-08 20:05:10 +0800

    fix(editor): keep text action listeners on long press

--- app/src/main/java/io/legado/app/ui/code/CodeEditActivity.kt
+++ app/src/main/java/io/legado/app/ui/code/CodeEditActivity.kt
@@ -26,6 +26,7 @@ import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.textfield.TextInputEditText
 import io.github.rosemoe.sora.event.ColorSchemeUpdateEvent
+import io.github.rosemoe.sora.event.InterceptTarget
 import io.github.rosemoe.sora.event.LongPressEvent
 import io.github.rosemoe.sora.event.PublishSearchResultEvent
 import io.github.rosemoe.sora.event.SelectionChangeEvent
@@ -185,7 +186,7 @@ class CodeEditActivity :
         editor.subscribeEvent(LongPressEvent::class.java) { event, _ ->
             val cursor = editor.cursor
             if (cursor.isSelected && event.index in cursor.left until cursor.right) {
-                event.intercept()
+                event.intercept(InterceptTarget.TARGET_EDITOR)
                 editor.postInLifecycle { actions.displayWindow() }
             }
         }
